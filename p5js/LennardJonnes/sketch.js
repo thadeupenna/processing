0880a8dfe1619f681f4e98390fc7e5a506000385
Thadeu Penna <thadeupenna@id.uff.br>
2020-08-31 14:13:51 -0300

    Fixed potential

--- p5js/LennardJonnes/sketch.js
+++ p5js/LennardJonnes/sketch.js
@@ -1,13 +1,22 @@
-let dt = 1e-3;
+let dt = 1e-2;
 let p;
+let whiteBoard;
+let xr = 100;
+let yr = 200;
+let r0 = 0;
+let v0 = 0;
+let sigma = 1;
+let e0 = 1;
+let rmin = 0.9,rmax = 2.5;
+let ymax= 0.05, ymin = -1;
 
 class Particle {
-
   constructor(x,d,m,E) {
     this.x = x;
     this.d = d;
     this.E = E;
-    this.v = sqrt(2*E/m); 
+    this.m = m;
+    this.v = sqrt(2*(this.E-ymin)/m); 
   }
 
   update() {
@@ -18,45 +27,70 @@ class Particle {
 
   show() {
     fill(255,0,255);
-    noStroke;
-    circle(this.x*80,this.E,this.d);
+    noStroke();
+    let xp = map(this.x,rmin,rmax,0,400);
+    let yp = map(this.E,ymax,ymin,0,400);
+    circle(xp,yp,this.d);
   } 
 }
 
 function LJphi(x) {
-  let sigma = 2;
-  let e0 = 1;
-  return 4*e0*pow(sigma/x,6)*(2*pow(sigma/x,6)-1.); 
+  return 4*e0*pow(sigma/x,6)*(pow(sigma/x,6)-1.); 
 }
 
-
 function LJf(x) {
-  sigma = 2;
-  let e0 = 100;
   return 24*e0*pow(sigma/x,6)*(2*pow(sigma/x,6)-1.)/x; 
 }
 
+function mudaEnergia() {
+  p.E = map(Esl.value(),0,10,v0,-0.1);
+  p.v = sqrt(2*(p.E-ymin)/p.m);
+  p.x = r0;
+  p.update();
+}
 
 function setup() {
   createCanvas(400, 400);
-  p = new Particle(3,10,1,0.00002);
+  whiteBoard = createGraphics(400,400);
+  whiteBoard.clear();
+  desenhaLJp();
+  createP('Energia');
+  Esl = createSlider(0,10,0);
+  Esl.input(mudaEnergia);
+  E = map(Esl.value(),0,10,v0,-0.1);
+  p = new Particle(r0, 10, 1, E);
+}
+
+function desenhaLJp() {
+  let x0 =0;
+  let y0 =0;
+  let zero = map(0,ymax,ymin,0,400);
+  whiteBoard.strokeWeight(1);
+  whiteBoard.stroke(0);
+  whiteBoard.line(0,zero,width,zero);
+  whiteBoard.strokeWeight(4);
+  whiteBoard.stroke(255,0,0);
+  for (let x=rmin; x<rmax; x+=0.01) {
+    let phi = LJphi(x);
+    let xp=map(x,rmin,rmax,0,400);
+    let yp=map(phi,ymax,ymin,0,400);
+    whiteBoard.line(x0,y0,xp,yp);  
+    x0 = xp;
+    y0 = yp;
+    if (phi < v0) {
+      v0 = phi;
+      r0 = x;  
+    }
+  }
+  whiteBoard.stroke(0);
+  whiteBoard.strokeWeight(1);
+  let x0p = map(r0,rmin,rmax,0,400);
+  whiteBoard.line(x0p,0,x0p,400);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  image(whiteBoard,0,0);
   p.update();
   p.show();
-  let rant=0;
-  let fant=400; 
-  stroke(255,0,0);
-  for (let r=0.1; r<10; r+=0.1) {
-    let newr = r*80;
-    let newf = 400+200*LJf(r);
-    line(rant,fant,newr,newf);  
-    rant = newr;
-    fant = newf;
-    console.log(rant,fant);
-  }
-  text(p.v,10,10);
-  text(LJf(p.x),10,30);
 }
