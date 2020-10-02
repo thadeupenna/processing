4e8eb72d37f46e780f7590648e01307f2e4145cc
Thadeu Penna <thadeupenna@id.uff.br>
2020-10-01 22:15:27 -0300

    Adicionei botões

--- p5js/FreeExpansion/sketch.js
+++ p5js/FreeExpansion/sketch.js
@@ -2,6 +2,7 @@ let height = 200;
 let width = 600;
 let gas = [];
 let cor_barreira;
+let barrier;
 
 
 class Particle {
@@ -19,32 +20,65 @@ class Particle {
   }
   
   move() {
+    let x0, xm;
+
     this.x = (this.x + this.vx);
     this.y = (this.y + this.vy);
-    if ((this.x >= barrier - this.r/2) || this.x<= this.r/2)  {
+    
+    if (this.x > barrier) {
+      x0 = barrier;         
+      xm = width;
+    }
+    else {
+      x0 = 0; 
+      xm = barrier;
+    }
+
+    if (this.x >= xm - this.r/2) {
       this.vx *= -1;
-      this.vx += randomGaussian(0,2);
+      this.x = xm - this.r/2
+    }  
+    if (this.x<= x0 + this.r/2)  {
+      this.vx *= -1;
+      this.x = x0 + this.r/2
     }
     if ((this.y >= height - this.r/2) || this.y<= this.r/2) {
       this.vy *= -1;
-      this.vy += randomGaussian(0,2);
     }
+  }
+}
+
+function Initia() {
+  for(let i=0; i<500; i++) {
+    gas[i] = new Particle(200,100,80);
   }
 }
 
 function setup() {
   createCanvas(width, height);
-  for(let i=0; i<500; i++) {
-    gas[i] = new Particle(200,100,80);
-  }
+  
   barrier = width/2;
   cor_barreira = color(255,0,0);
+  let btnClose = createButton("Fecha");
+  btnClose.mousePressed(Fecha);
+
+  let btnOpen = createButton("Abre");
+  btnOpen.mousePressed(Libera);
+
+  let btnReset = createButton("Inicia");
+  btnReset.mousePressed(Initia);
+  Initia();
   frameRate(40);
 }
 
-function mousePressed() {
+function Libera() {
   barrier = width;
   cor_barreira.setAlpha(0);
+} 
+
+function Fecha() {
+  barrier = width/2;
+  cor_barreira.setAlpha(255);
 } 
 
 function draw() {
@@ -58,5 +92,4 @@ function draw() {
     p.move();
     p.show();
   }
-  
 }
